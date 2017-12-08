8e949952b9849d2e125a9d0d75226694889288d8
enzhao <enzhao@staff.weibo.com>
2017-12-08 16:36:10 +0800

    [ENH] json to xml half done.

--- joyCodes/json2XML.py
+++ joyCodes/json2XML.py
@@ -3,6 +3,9 @@ from collections import Iterable
 def json2dict(_json_str = ""):
     return json.loads(_json_str)
 
+def dict2json(_dict = {}):
+    return json.dumps(_dict)
+
 def mk_str(_obj = Iterable, _delimiter = ",", _indentation = ""):
     items_value = ""
     if len(_obj) < 1 : return items_value
@@ -17,7 +20,7 @@ def mk_str(_obj = Iterable, _delimiter = ",", _indentation = ""):
         items_value += str(_obj[-1]) + "\n"
     return items_value
 
-def dict2xml(_json_dict = [], _indentation = ""):
+def dict2xml(_json_dict = {}, _indentation = ""):
     rst_str = ""
     for key in _json_dict.keys():
         value = _json_dict.get(key)
@@ -35,7 +38,7 @@ def dict2xml(_json_dict = [], _indentation = ""):
 
 lint = [1,2,3]
 lstr = ["1","2",'3']
-req_dict = '{"message":"success","result":[{"timestamp":"1511858444","id":"1","other":"{}","importance":"0","url":"","size":"1024","version":"1.0.0"}],"params":{},"code":200}'
+req_dict = json.loads('{"message":"success","result":[{"timestamp":"1511858444","id":"1","other":"{}","importance":"0","url":"","size":"1024","version":"1.0.0"}],"params":{},"code":200}')
 dd = {"req":req_dict,"lint":lint,"lstr":lstr,"int":3,"str":"asddiid"}
 print dict2xml(dd)
 
